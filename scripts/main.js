84ce5517b5241bbb75709b4d79191d87984d877e
Nora <noraniederer1@Noras-MacBook-Air.local>
2019-10-05 18:40:15 +0200

    commit  - new menu test

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+// < !--script for dropdown menu-- >
+
+// Click on icon to toggle between adding and removing the "responsive" class to topnav
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// dropdown - submenu
+// toggle between hiding and showing the dropdown content
+
+// Click to show dropdown submenu1 - angebot
+function myFunction1() {
+  document.getElementById("myDropdown1").classList.toggle("show");
+}
+
+//Click to show dropdown submenu2 - fotografie
+function myFunction2() {
+  document.getElementById("myDropdown2").classList.toggle("show");
+}
+
+//Click to show dropdown submenu3 - natur & kunst
+function myFunction3() {
+  document.getElementById("myDropdown3").classList.toggle("show");
+}
