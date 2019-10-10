849fa6f735bd9e3ebfb64158d972eb88d7898ee0
Nora <noraniederer1@Noras-MacBook-Air.local>
2019-10-10 17:36:51 +0200

    commit - organized js stuff

---
+++ scripts/read-more.js
@@ -0,0 +1,52 @@
+// read-more buttons
+
+// read more button 1
+function myFunction1() {
+  var dots = document.getElementById("dots1");
+  var moreText = document.getElementById("more1");
+  var btnText = document.getElementById("myBtn1");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "mehr lesen";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "weniger lesen";
+    moreText.style.display = "inline";
+  }
+}
+
+// read more button 2
+function myFunction2() {
+  var dots = document.getElementById("dots2");
+  var moreText = document.getElementById("more2");
+  var btnText = document.getElementById("myBtn2");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "mehr lesen";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "weniger lesen";
+    moreText.style.display = "inline";
+  }
+}
+
+// read more button 3
+function myFunction3() {
+  var dots = document.getElementById("dots3");
+  var moreText = document.getElementById("more3");
+  var btnText = document.getElementById("myBtn3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "mehr lesen";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "weniger lesen";
+    moreText.style.display = "inline";
+  }
+}
